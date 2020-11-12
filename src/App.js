d2e55a1a618aef55d9380c3209fe2ce49dc8339c
Ryan Caplette <ryancaplette@gmail.com>
2020-11-12 15:08:22 -0500

    snapshot of cognito login

--- src/App.js
+++ src/App.js
@@ -1,101 +1,60 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/analytics';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+Amplify.configure(awsconfig);
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+let user = false;
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBqsuVKe1iFlUwI03S645YBChtggQqdbjA",
-  authDomain: "ryanchat-com.firebaseapp.com",
-  databaseURL: "https://ryanchat-com.firebaseio.com",
-  projectId: "ryanchat-com",
-  storageBucket: "ryanchat-com.appspot.com",
-  messagingSenderId: "682249773571",
-  appId: "1:682249773571:web:2bab58bee89de0d98ca436"
+Auth.currentUserInfo().then((data) => {
+  user = data;
+  console.log(data)
 });
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-const analytics = firebase.analytics();
-
-
 function App() {
-
-  const [user] = useAuthState(auth);
 
   return (
     <div className="App">
       <header className="App-header">
         <h4>Welcome to Ryan.chat - So anyone can contact me. 😊</h4>
-        <SignOut />
+        <AmplifySignOut />
       </header>
       <section>
-        {user ? <ChatRoom /> : <SignIn />}
+        {/* this doesnt work would need way to update state and display value  */}
+        I'm Logged In!
       </section>
     </div>
   );
 }
 
 function SignIn() {
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
 
   return (
-    <button onClick={signInWithGoogle}>Sign In</button>
+    <div>
+    My App
+    <AmplifySignOut />
+  </div>
   );
-}
-
-function SignOut() {
-  return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
-  )
 }
 
 function ChatRoom() {
 
   const scrollHelper = useRef();
 
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limitToLast(25);
-
-  const [messages] = useCollectionData(query, {idField: 'id'});
 
   const [formValue, setFormValue] = useState('');
-
-  const sendMessage = async(e) => {
-    e.preventDefault();
-
-    const { uid, photoURL } = auth.currentUser;
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL
-    });
-
-    setFormValue('');
-
-    scrollHelper.current.scrollIntoView({ behavior: 'smooth'});
-
-  }
 
   return (
     <>
       <main>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <div> Hello Moto </div>
 
         <div ref={scrollHelper}></div>
       </main>
 
-      <form onSubmit={sendMessage}>
+      <form>
         <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
         <button type="submit">Submit</button>
       </form>
@@ -104,17 +63,4 @@ function ChatRoom() {
 
 }
 
-function ChatMessage(props) {
-  const {text, uid, photoURL } = props.message;
-
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-
-  return (
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL} />
-      <p>{text}</p>
-    </div>
-  )
-}
-
-export default App;
+export default withAuthenticator(App);
